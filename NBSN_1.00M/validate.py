0f5ba383814b9c097603ee60f4fe96fca02186d9
HJK2017 <hyunjun0.kim@snu.ac.kr>
2025-03-03 22:28:45 +0900

    Delete NBSN_1.00M/validate.py

--- NBSN_1.00M/validate.py
+++
@@ -1,8 +0,0 @@
-import json
-from preparation import validate
-
-with open("./config.json", "r") as f:
-    config = json.load(f)
-
-if __name__ == "__main__":
-    validate()                 # Check the PSNR and SSIM value for SIDD Validation
